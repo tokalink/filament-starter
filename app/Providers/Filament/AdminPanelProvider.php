ea199c9d8132432dfa2faa5005511aa51f5d8e59
tokalink <tokalinkindonesia@gmail.com>
2025-02-09 12:24:04 +0700

    update sidebar navigation background to gradient and modify admin panel provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,8 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -28,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->spa()
             ->login()
+            // ->topNavigation()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -51,6 +54,10 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+            ])
+            // add menu logout di sidebar
+            ->navigationItems([
+               
             ])
             ->authMiddleware([
                 Authenticate::class,
