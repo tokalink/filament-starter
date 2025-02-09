93743ac7a9f2ffa559d245cc4cf0a868f3de4199
tokalink <tokalinkindonesia@gmail.com>
2025-02-09 10:34:40 +0700

    init filament

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        FilamentAsset::register([
+            Css::make('custom', __DIR__ . '/../../resources/css/custom.css'), //->loadedOnRequest(),
+        ]);
+    }
+}
---
+++ setup.sh
@@ -0,0 +1,6 @@
+composer require laravel/octane
+
+php artisan octane:install --server=frankenphp
+
+# Start the server
+#php artisan octane:frankenphp --watch --port=8333 --admin-port=2333
